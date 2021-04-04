4ee0b88eb0e8209b09d3cbdb35cfe175467d6995
Abdul Rahman <76507583+abdrhmn1601@users.noreply.github.com>
2021-04-04 12:13:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const content = document.getElementById('content');
 const footer = document.getElementsByTagName('footer')[0];
 const timer = document.getElementById('timer');
 
-const detik = 1000,
-  menit = detik * 60,
-  jam = menit * 60,
-  hari = jam * 24;
+const second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
 let countDown = new Date('April 07, 2021 00:00:00').getTime(),
   x = setInterval(function () {
     let now = new Date().getTime(),
       distance = countDown - now;
 
     // document.getElementById('days').innerText = Math.floor(distance / (day)),
-    document.getElementById('jam').innerText = Math.floor(distance / (jam)),
-      document.getElementById('menit').innerText = Math.floor((distance % (jam)) / (menit)),
-      document.getElementById('detik').innerText = Math.floor((distance % (menit)) / detik);
+    document.getElementById('hours').innerText = Math.floor(distance / (hour)),
+      document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+      document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
     if (distance < 0) {
 
